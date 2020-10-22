e0bdf3a4780fcb3b5120906b0bd8fa8d0d5df0ad
wojciesfpw <73296670+wojciesfpw@users.noreply.github.com>
2020-10-22 18:24:29 +0200

    Add files via upload

---
+++ zad1/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char * argv[]) {
+    if (argv[1] != NULL)
+    {
+        float a = atof(argv[1]);
+        printf("Liczba %f podniesiona do kwadratu wynosi %f", a, a*a);
+    } else {
+        printf("Nie podano żadnej wartości");
+    }
+    
+    return 0;
+}
